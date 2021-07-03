914470eda6fbd77ccefacc34e0054066022d00f4
Steven Gentens <stg@foreach.be>
2021-07-03 22:14:40 +0200

    simplify quality changes logic by reducing checks and slightly more readable branches

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,42 +9,46 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!isAgedBrie(items[i]) && !isBackstagePasses(items[i])) {
-                if (items[i].quality > 0 && !isSulfuras(items[i])) {
-                    items[i].quality = items[i].quality - 1;
+            Item item = items[i];
+            if (!isAgedBrie(item) && !isBackstagePasses(item)) {
+                if (item.quality > 0 && !isSulfuras(item)) {
+                    item.quality = item.quality - 1;
                 }
-            } else if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
-
-                if (isBackstagePasses(items[i]) && items[i].quality < 50) {
-                    if (items[i].sellIn < 11) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-
-                    if (items[i].sellIn < 6) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+            } else if (item.quality < 50) {
+                item.quality = item.quality + 1;
+                if (isBackstagePasses(item)) {
+                    handleBackstagePasses(item);
                 }
             }
 
-            if (!isSulfuras(items[i])) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!isSulfuras(item)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!isAgedBrie(items[i])) {
-                    if (!isBackstagePasses(items[i])) {
-                        if (items[i].quality > 0 && !isSulfuras(items[i])) {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    } else {
-                        items[i].quality = 0;
-                    }
-                } else if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            if (isAfterSellDate(item)) {
+                if (isAgedBrie(item) && item.quality < 50) {
+                    item.quality = item.quality + 1;
+                } else if (isBackstagePasses(item)) {
+                    item.quality = 0;
+                } else if (!isSulfuras(item)) {
+                    item.quality = item.quality > 0 ? item.quality - 1 : 0;
                 }
             }
         }
+    }
+
+    private void handleBackstagePasses(Item item) {
+        if (item.sellIn < 11) {
+            item.quality = item.quality + 1;
+        }
+
+        if (item.sellIn < 6) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private boolean isAfterSellDate(Item item) {
+        return item.sellIn < 0;
     }
 
     private boolean isAgedBrie(Item item) {
