3672c8a24c29a5e2e97931a60c8280a723d8148e
Steven Gentens <stg@foreach.be>
2021-07-06 01:52:19 +0200

    Added SulfurasItemProxy

---
+++ src/main/java/com/gildedrose/proxy/SulfurasItemProxy.java
@@ -0,0 +1,30 @@
+package com.gildedrose.proxy;
+
+import com.gildedrose.Item;
+
+/**
+ * @author Steven Gentens
+ * @since 0.0.1
+ */
+public final class SulfurasItemProxy extends ItemProxy {
+
+    SulfurasItemProxy(Item item) {
+        super(item);
+    }
+
+    @Override
+    public void incrementSellInAndUpdateQuality() {
+        // never needs to be sold or degrades in quality
+    }
+
+    @Override
+    void nextDay() {
+        // never sold
+    }
+
+    @Override
+    int calculateNextQualityDegradation() {
+        // never degrades in quality
+        return 0;
+    }
+}
---
+++ src/test/java/com/gildedrose/proxy/SulfurasItemProxyTest.java
@@ -0,0 +1,57 @@
+package com.gildedrose.proxy;
+
+import com.gildedrose.Item;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static com.gildedrose.utils.GildedRoseTestUtils.verifySellInAndQuality;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Steven Gentens
+ * @since 0.0.1
+ */
+public class SulfurasItemProxyTest {
+
+    @ParameterizedTest
+    @CsvSource({
+        "40,40",
+        "50,50",
+        "48,48",
+        "10,10",
+        "10,10",
+        "0,0",
+        "3,3"
+    })
+    void neverIncrementSellInOrUpdateQuality(int quality, int expectedQuality) {
+        Item item = new Item("test", 5, quality);
+        ItemProxy proxy = new SulfurasItemProxy(item);
+        proxy.incrementSellInAndUpdateQuality();
+        verifySellInAndQuality(item, 5, expectedQuality);
+    }
+
+
+    @Test
+    void neverIncrementsNextDay() {
+        Item item = new Item("test", 5, 10);
+        ItemProxy proxy = new SulfurasItemProxy(item);
+        proxy.nextDay();
+        verifySellInAndQuality(item, 5, 10);
+    }
+
+    @Test
+    void neverUpdateQuality() {
+        Item item = new Item("test", 5, 10);
+        ItemProxy proxy = new SulfurasItemProxy(item);
+        proxy.updateQuality();
+        verifySellInAndQuality(item, 5, 10);
+    }
+
+    @Test
+    void neverChangesQuality() {
+        Item item = new Item("test", 5, 10);
+        ItemProxy proxy = new SulfurasItemProxy(item);
+        assertEquals(0, proxy.calculateNextQualityDegradation());
+    }
+}
